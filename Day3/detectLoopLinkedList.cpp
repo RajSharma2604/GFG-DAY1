45bff29743dbdaf461adaf8cee90e2a77c31d7c3
RajSharma2604 <rajsharma240401@gmail.com>
2024-01-25 23:03:13 +0530

    remove-loop

---
+++ Day3/detectLoopLinkedList.cpp
@@ -0,0 +1,21 @@
+ //linked list head node is passed
+ 
+ bool detectLoop(Node* head)
+    {
+       
+        //edge case
+        if(head==NULL ||head->next==NULL)return false;
+        if(head==head->next)return true;
+        if(head->next->next==head)return true;
+        Node *slow=head;
+        Node *fast=head;
+        while(fast && fast->next)
+        {
+           
+            slow=slow->next;
+            fast=fast->next->next;
+             if(slow==fast)return true;
+        }
+        return false;
+    }
+    
