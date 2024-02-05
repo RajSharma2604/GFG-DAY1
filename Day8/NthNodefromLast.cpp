08c2023ff17987666fa9116cafec01766183e744
RajSharma2604 <rajsharma240401@gmail.com>
2024-02-05 23:49:47 +0530

    LINKEDLIST

---
+++ Day8/NthNodefromLast.cpp
@@ -0,0 +1,29 @@
+//
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+    int getNthFromLast(Node *head, int n)
+    {
+           // Your code here
+           //edge case
+           if(n==0)return -1;
+           Node *p=head;
+           int count=0;
+           while(p)
+           {
+               p=p->next;
+               count++;
+           }
+           if(count<n)return -1;
+           int k=count-n;
+           p=head;
+           while(k--)
+           {
+               p=p->next;
+           }
+           return p->data;
+    }
+int main()
+{
+return 0;
+}
