85e9083f58a0ac8d88fad49ba9e1461380b55dbf
RajSharma2604 <rajsharma240401@gmail.com>
2024-02-06 23:55:36 +0530

    dell questions

--- DELL/dell4.cpp
+++ DELL/dell4.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 #include<bits/stdc++.h>
 int main()
-{
+{   
+    cout<<"enter a number :"<<endl;
+    int n;
+    int flag=0;
+    cin>>n;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)cout<<"Not a prime number:";
+    else cout<<"Prime number ,need to calculate factroial for it:"<<endl;
 return 0;
 }
